7e55f67a6ca094ac536b036617a5d9aecfbd550d
VatsalyaGautam <vatsalya@ssipmt.com>
2025-10-18 17:01:14 +0530

    update : animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,38 @@
 module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
-
     screens: {
-      
-      desk: '1280px',
-      tab:'800px',
-      mob:'375px'
-     
+      desk: "1280px",
+      tab: "800px",
+      mob: "375px",
     },
     extend: {
-      fontFamily: {
-        crimson: ['"Crimson Text"', 'serif'],
-        dm: ['"DM Sans"', 'sans-serif'],
-        mono:[' "Roboto Mono" ', 'monospace'],
-        rethink : [' "Rethink Sans" ', 'serif' ],
-        reddit : [' "Reddit Mono" ', 'monospace']
+      keyframes: {
+        slideUp: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(40px) scale(0.95)",
+            filter: "blur(4px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0) scale(1)",
+            filter: "blur(0px)",
+          },
+        },
+      },
+      animation: {
+        slideUp: "slideUp 1.75s cubic-bezier(0.16, 1, 0.3, 1) forwards",
       },
 
+      fontFamily: {
+        crimson: ['"Crimson Text"', "serif"],
+        dm: ['"DM Sans"', "sans-serif"],
+        mono: [' "Roboto Mono" ', "monospace"],
+        rethink: [' "Rethink Sans" ', "serif"],
+        reddit: [' "Reddit Mono" ', "monospace"],
+      },
     },
   },
   plugins: [],
-}
-
+};
